da8479514877a01d1f618d59c77686cbfcb272fd
sukitha <sukithaj@gmail.com>
2015-05-05 15:19:33 +0530

    loglevel parameter added

--- LogHandler/CommonLogHandler.js
+++ LogHandler/CommonLogHandler.js
@@ -12,28 +12,33 @@ var WriteLog = funnpm install ction(message)
 
 module.exports.WriteLog = WriteLog;
 */
-
+//winston.addColors({debug: 'green',info:  'cyan',silly: 'magenta',warn:  'yellow',error: 'red'});
 
  var logger = new winston.Logger();
 
- if (process.env.DEPLOYMENT_ENV == 'docker') {
+var level = 'debug';
 
-     logger.add(winston.transports.Console, {colorize: true});
+if(process.env.LOG_LEVEL) {
+    level = process.env.LOG_LEVEL;
+}
+
+if (process.env.DEPLOYMENT_ENV == 'docker') {
+
+     logger.add(winston.transports.Console, {colorize: true, level: level});
      //logger.add(winston.transports.File, { filename: 'logger.log' });
 
  }
  else {
 
-     logger.add(winston.transports.Console, {colorize: true});
+     logger.add(winston.transports.Console, {colorize: true, level: level});
 
      if(process.env.LOG_PATH) {
-         logger.add(winston.transports.File, {filename: process.env.LOG_PATH + '/logger.log'});
+         logger.add(winston.transports.File, {filename: process.env.LOG_PATH + '/logger.log', level: level});
      }else{
-         logger.add(winston.transports.File, {filename: 'logger.log'});
+         logger.add(winston.transports.File, {filename: 'logger.log', level: level});
 
      }
  }
-
 
 
 if(process.env.LOG_SERVER && process.env.LOG_PORT){
@@ -42,7 +47,8 @@ if(process.env.LOG_SERVER && process.env.LOG_PORT){
     winston.add(winston.transports.LogstashUDP, {
         port: process.env.LOG_PORT,
         appName: process.env.HOST_NAME,
-        host: LOG_SERVER
+        host: LOG_SERVER,
+        level: level
     });
 
 }
