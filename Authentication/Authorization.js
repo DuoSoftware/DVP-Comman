cab44a008062c7efab235cb8f41859894d93773b
sukitha <sukithaj@gmail.com>
2016-03-01 12:49:54 +0530

    global company info resolve

--- Authentication/Authorization.js
+++ Authentication/Authorization.js
@@ -4,55 +4,85 @@ module.exports = function(options) {
 //{resource:"cluster", action:"read"}//
 
 
-
-
     var middleware = function(req, res, next) {
 
         var resource = options.resource;
         var action = options.action;
-        if(resource) {
-            var selected = req.user.scope.filter(function(item){
-
-                return item.resource == resource;
-
-            });
-
-            if (selected && selected.length > 0) {
-
-                var actions = selected[0].actions;
-                if(action){
-
-                    var index1 = actions.indexOf(action);
-                    if(index1 > -1){
-
-                        next();
 
 
-                    }else{
+        ///////////////////////fix company info/////////////////////////////////////////////////////////////////////////////////
+        if(req.user.company && req.user.tenant && req.user.company == -1 && req.user.tenant == -1 && req.headers['companyinfo'] ){
+
+            var arr = req.headers['companyinfo'].split(":");
+            if(arr.length > 1){
+
+                req.user.tenant =  arr[0];
+                req.user.company =  arr[1];
+            }
+        }
+
+
+        if(req.user.scope ) {
+
+
+            var index = req.user.scope.indexOf("all");
+
+            if(index > 0){
+
+                next();
+
+            }else {
+
+
+                if (resource) {
+                    var selected = req.user.scope.filter(function (item) {
+
+                        return item.resource == resource;
+
+                    });
+
+                    if (selected && selected.length > 0) {
+
+                        var actions = selected[0].actions;
+                        if (action) {
+
+                            var index1 = actions.indexOf(action);
+                            if (index1 > -1) {
+
+                                next();
+
+
+                            } else {
+
+                                next(new Error('insufficient scopes'));
+
+                            }
+
+
+                        } else {
+                            next();
+
+                        }
+
+
+                    } else {
 
                         next(new Error('insufficient scopes'));
 
                     }
+                } else {
 
-
-                }else{
                     next();
 
+
                 }
-
-
-            } else {
-
-                next(new Error('insufficient scopes'));
-
             }
+
         }else{
 
-            next();
-
+            next(new Error('insufficient scopes'));
 
         }
-
 
 
     };
